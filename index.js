67392f54a6c0e611f3dbc8cc7030c553a6c3e3a3
RakibulHasan2 <rakibulhasan99445@gmail.com>
2022-11-08 18:41:36 +0600

    data loaded

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 //middle ware
 app.use(cors())
@@ -16,11 +16,17 @@ async function run(){
     try{
          const serviceCollection = client.db('trueCare').collection('services')
 
-         app.get('/service', async( req, res) =>{
+         app.get('/services', async( req, res) =>{
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services)
+         })
+         app.get('/services/:id', async( req, res) =>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
          })
     }
     finally{
